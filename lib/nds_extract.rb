e1728e47ffa742223214879776365d55dc6886f4
Robert Phillips <rpdecks@gmail.com>
2020-03-17 18:32:18 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -24,8 +24,8 @@ def directors_totals(nds)
   result = {}
   director_index = 0
   while director_index < nds.length do
-    director_data = nds[director_index][:name]
-    result[director_data] = gross_for_director(director_data)
+    director_name = nds[director_index][:name]
+    result[director_name] = gross_for_director(director_data)
     director_index += 1
   end
   result

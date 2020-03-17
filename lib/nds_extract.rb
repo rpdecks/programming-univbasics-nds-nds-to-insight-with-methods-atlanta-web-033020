5e6165f9abd75c0008de6786e7dc8e7de59e45cb
Robert Phillips <rpdecks@gmail.com>
2020-03-17 18:10:02 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -18,3 +18,23 @@ def directors_totals(nds)
   result = {}
   nil
 end
+
+def directors_totals(nds)
+  result = {
+    
+  }
+
+  director_index = 0
+  while director_index < nds.length do
+    director_name = nds[director_index][:name]
+    result[director_name] = 0
+    movie_index = 0
+    #binding.pry
+    while movie_index < nds[director_index][:movies].length do
+      result[director_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
+      movie_index += 1
+    end
+    director_index += 1
+  end
+  result
+end
